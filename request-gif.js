84f4aa38bd4edfbf96ce5acfce848f86d39937fe
Elizabeth Niebauer <egniebauer@gmail.com>
2017-08-25 11:53:01 -0500

    beautify: add bootstrap

--- request-gif.js
+++ request-gif.js
@@ -6,27 +6,41 @@ $(document).ready(function() {
 
 
 function clearPreviousValidation() {
-  var $riddle = $('#form-gif-request div input');
-  var $hammer = $("#error");
-
-  $riddle.removeClass('invalid');
-  $hammer.removeClass('invalid').attr("hidden", true);
+  $('#gif-well').attr('hidden', true);
+  $('#gif').prop('src', "");
+  $("#feedback").text("");
+  $('#validation').removeClass('has-error');
+  $('#error').remove();
 }
 
+
+function addErrorAlert() {
+  var errorAlert = '<div class="row">' +
+        '<div class="col-sm-6 col-sm-offset-3">' +
+          '<div id="error" class="alert alert-danger alert-dismissible" role="alert">' +
+            '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+            '<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>' +
+            '<span class="sr-only">Error:</span> No gifs for you!' +
+          '</div>' +
+        '</div>' +
+      '</div>';
+
+  $('button').after(errorAlert);
+}
 
 function validateForm(event) {
   event.preventDefault();
   clearPreviousValidation();
 
-  var $riddle = $('#form-gif-request div input');
-
-  if ($riddle.val() == 5) {
+  if ($('#valid-jacksons').val() == 5) {
+    $('#gif-well').attr('hidden', false);
     fetchAndDisplayGif(event);
   } else {
-    $("#error").text("No gifs for you!").attr("hidden", false).addClass('invalid');
-    $riddle.addClass('invalid');
+    $('#validation').addClass('has-error');
+    addErrorAlert();
   }
 }
+
 
 /**
  * sends an asynchronous request to Giphy.com aksing for a random GIF using the
@@ -35,7 +49,7 @@ function validateForm(event) {
  * upon receiving a response from Giphy, updates the DOM to display the new GIF
  */
 function fetchAndDisplayGif(event) {
-    var searchQuery = $('#form-gif-request input').val();
+    var searchQuery = $('#tag').val();
     var params = {
         api_key: config.GIPHY_KEY,
         tag : "jackson 5 " + searchQuery
